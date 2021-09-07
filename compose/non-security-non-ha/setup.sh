3220b9c2e8e776a591f689b281c1a1ce9c3e6cf0
codewithaloknath <mail2aloknath@gmail.com>
2021-09-07 11:41:17 +0530

    Update setup.sh

--- compose/non-security-non-ha/setup.sh
+++ compose/non-security-non-ha/setup.sh
@@ -8,7 +8,7 @@ cd ../hive/
 wget https://downloads.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
 
 cd ../olk/
-wget https://gitee.com/openlookeng/hetu-core.git
+wget https://download.openlookeng.io/1.3.0/hetu-server-1.3.0.tar.gz
 
 cd ..
 
@@ -16,4 +16,4 @@ path=`pwd`
 echo export HADOOP_PACKAGE=$path/hadoop/hadoop-3.3.0.tar.gz >> config
 echo export DERBY_PACKAGE=$path/derby/db-derby-10.14.2.0-bin.tar.gz >> config
 echo export HIVE_PACKAGE=$path/hive/apache-hive-3.1.2-bin.tar.gz >> config
-echo export OLK_PACKAGE=$path/olk/hetu-core.git >> config
+echo export OLK_PACKAGE=$path/olk/hetu-server-1.3.0.tar.gz >> config
