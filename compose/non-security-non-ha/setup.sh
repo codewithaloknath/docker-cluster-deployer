d3bac3dc31158a28caa02bb5582a5f7e0ac4abac
codewithaloknath <mail2aloknath@gmail.com>
2021-09-07 09:37:22 +0530

    Create setup.sh

---
+++ compose/non-security-non-ha/setup.sh
@@ -0,0 +1,19 @@
+cd hadoop
+wget https://archive.apache.org/dist/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
+
+cd ../derby/
+wget https://archive.apache.org/dist/db/derby/db-derby-10.14.2.0/db-derby-10.14.2.0-bin.tar.gz
+
+cd ../hive/
+wget https://downloads.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
+
+cd ../olk/
+wget https://gitee.com/openlookeng/hetu-core.git
+
+cd ..
+
+path=`pwd`
+echo export HADOOP_PACKAGE=$path/hadoop/hadoop-3.3.0.tar.gz >> config
+echo export DERBY_PACKAGE=$path/derby/db-derby-10.14.2.0-bin.tar.gz >> config
+echo export HIVE_PACKAGE=$path/hive/apache-hive-3.1.2-bin.tar.gz >> config
+echo export OLK_PACKAGE=$path/olk/hetu-core.git >> config
